2b6332a8ac4f554b96ffb703515ca15ad9cee528
Edward <spottablefiend@gmail.com>
2018-03-20 17:29:24 +0000

    First commit. essentially my progress on ESS that applies here

---
+++ splatterFest/Assets/Scripts/playerLook.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerLook : MonoBehaviour
+{
+
+    GameObject playerBody;
+    public float mouseSensitivity;
+    bool seenObject;
+
+    public Text seenObjectText;
+
+    float xAxisClamp = 0.0f;
+
+    private void Awake()
+    {
+        //Lock cursor to centre of screen
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBody = GameObject.FindGameObjectWithTag("playerBody");
+
+        seenObject = false;
+        seenObjectText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateView();
+        lookAtObject();
+    }
+    void rotateView()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * mouseSensitivity;
+        float rotAmountY = mouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotationCamera = transform.rotation.eulerAngles;
+        Vector3 targetRotationBody = playerBody.transform.rotation.eulerAngles;
+
+        targetRotationCamera.x -= rotAmountY; //Rotatates camera in direction of cursor up and down
+        targetRotationCamera.z = 0; //Stops over rotation;
+        targetRotationBody.y += rotAmountX; // Rotates whole body to make movement easier
+
+
+        // Clamps the camera in the x between -90 and 90 (directly up and directly down)
+        //To stop flicker
+        if (xAxisClamp > 90)
+        {
+            xAxisClamp = targetRotationCamera.x = 90;
+        }
+        else if (xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotationCamera.x = 270;
+        }
+
+        transform.rotation = Quaternion.Euler(targetRotationCamera);
+        playerBody.transform.rotation = Quaternion.Euler(targetRotationBody);
+    }
+
+
+    void lookAtObject()
+    {
+        Ray ray = GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f)); //Aligns ray to centre of cam
+        RaycastHit objectHit;
+      
+        seenObject = Physics.Raycast(ray, out objectHit, 5.0f);
+        
+
+        if (seenObject)
+        {
+            seenObjectText.GetComponent<Text>().text = objectHit.collider.tag.ToString();
+            seenObjectText.gameObject.SetActive(true);
+
+
+
+            if (Input.GetButton("Fire1"))
+            {
+                objectHit.transform.SetParent(gameObject.transform);
+
+                if (objectHit.transform.GetComponent<Rigidbody>())
+                {
+                    objectHit.transform.GetComponent<Rigidbody>().useGravity = false;
+                }
+
+
+            }
+            else
+            {
+                objectHit.transform.SetParent(null);
+
+                if (objectHit.transform.GetComponent<Rigidbody>())
+                {
+                    objectHit.transform.GetComponent<Rigidbody>().useGravity = true;
+                }
+            }
+        }
+        else
+        {
+            seenObjectText.gameObject.SetActive(false);
+            
+
+        }
+
+
+        }
+    }
+
+
---
+++ splatterFest/Assets/Scripts/playerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    private CharacterController playerController;
+    
+    public float speed;
+
+
+    private void Awake()
+    {
+        playerController = GetComponent<CharacterController>();
+       
+    }
+
+    private void Update()
+    {
+        movement();
+    }
+
+    void movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //Movement
+
+        //Move side to side
+        Vector3 moveHorizontal = transform.right * horizontal * speed * Time.deltaTime;
+
+        //Move forward and back
+        Vector3 moveForward = transform.forward * vertical * speed * Time.deltaTime;
+
+        //Implement said moves
+        playerController.SimpleMove(moveHorizontal);
+        playerController.SimpleMove(moveForward);
+    }
+
+  
+}
